4f7e9f836d1de443624bffc2315b7be498750f27
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-14 11:49:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
+def WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5]
   [6,7,8]
@@ -14,3 +14,4 @@ WIN_COMBINATIONS = [
   [0,4,8]
   [6,4,2]
 ]
+end
